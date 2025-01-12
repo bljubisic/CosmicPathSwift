73c8bf527fe92b873063842ce6bb64a0f2cadcbe
Bratislav Ljubisic <bratislav.ljubisic@icloud.com>
2025-01-12 13:16:26 +0100

    Initial Commit

---
+++ CosmicPathSwift/CosmicPathSwiftApp.swift
@@ -0,0 +1,32 @@
+//
+//  CosmicPathSwiftApp.swift
+//  CosmicPathSwift
+//
+//  Created by Bratislav Ljubisic Home  on 1/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CosmicPathSwiftApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CosmicPathSwiftTests/CosmicPathSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicPathSwiftTests.swift
+//  CosmicPathSwiftTests
+//
+//  Created by Bratislav Ljubisic Home  on 1/12/25.
+//
+
+import Testing
+@testable import CosmicPathSwift
+
+struct CosmicPathSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
